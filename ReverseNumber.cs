431cc1b3c3d756ac3e26cd9f0a03f684e58b480d
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-10 15:33:33 +0530

    reverse a number

---
+++ ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Print1ToN
+{
+    internal class ReverseNumber
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter a number to reverse");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int rev=0;
+        //    do
+        //    {
+        //        int d = n % 10;
+        //        rev = rev * 10 + d;
+        //        n = n / 10;
+        //    } while (n > 0);
+        //    Console.WriteLine(rev);
+        //}
+    }
+}
