5ed6f783207c079493186c87b1cbf53618a548e8
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-10 15:32:34 +0530

    sum of digits

---
+++ FindSumOfDigitsOfNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Print1ToN
+{
+    internal class FindSumOfDigitsOfNumber
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter the number");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int sum = 0;
+        //    do
+        //    {
+        //        int d = n % 10;
+        //        sum = sum + d;
+        //        n = n / 10;
+        //    } while (n > 0);
+
+        //    Console.WriteLine(sum);
+        //}
+
+    }
+}
