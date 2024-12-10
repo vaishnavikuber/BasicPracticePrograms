0e24ae58f42413fb7051cb4ee8e9eb1f502d60e5
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-10 15:31:07 +0530

    factorial program

---
+++ FactorialOfNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Print1ToN
+{
+    internal class FactorialOfNumber
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter number");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int fact = 1;
+        //    for(int i=n; i>0; i--)
+        //    {
+        //        fact = fact * i;
+        //    }
+        //    Console.WriteLine("FactorialOfNumber of" + n + " is " + fact);
+
+        //}
+    }
+}
