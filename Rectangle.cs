fd0145f12ad93c8b35a4610576c8d43baf96be7a
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2024-12-10 17:46:06 +0530

    area of rectangle

---
+++ Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Print1ToN
+{
+    internal class Rectangle
+    {
+        static void area(int length,int width)
+        {
+
+            Console.WriteLine("The area of rectangle is "+(length*width));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width");
+            int width = int.Parse(Console.ReadLine());
+
+            area(length, width);
+            Console.ReadLine();
+        }
+
+    }
+}
