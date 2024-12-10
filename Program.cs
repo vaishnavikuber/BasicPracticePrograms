dcdecda90f4e8996345ee6aa4511cc41c19034e6
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-10 15:34:32 +0530

    print number from 1 to N

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Print1ToN
+{
+    public class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int n;
+        //    Console.WriteLine("Enter the number");
+        //    n =  Convert.ToInt32(Console.ReadLine());
+        //    for(int i=1 ; i <= n; i++)
+        //    {
+        //        Console.WriteLine(i);
+
+        //    }
+        //    Console.WriteLine();
+        //}
+    }
+}
